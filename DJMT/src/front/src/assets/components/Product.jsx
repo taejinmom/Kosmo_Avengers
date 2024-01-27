afa7379339ae6349f245e534ef9d270809184801
MIREU-CHOI <alfmsp123@naver.com>
2024-01-27 12:33:39 +0900

    product test

---
+++ DJMT/src/front/src/assets/components/Product.jsx
@@ -0,0 +1,23 @@
+import {useEffect, useState, React, Link} from "react";
+import { skillText } from "../constants";
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import Button from 'react-bootstrap/Button';
+
+// useEffect(() => {
+//     axios
+// }, []);
+
+const Skill = () => {
+  return (
+  <>
+
+  <main>
+  <div>
+  test
+  </div>
+  </main>
+  </>
+  );
+};
+
+export default Skill;
---
+++ DJMT/src/front/src/assets/views/components/Product.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+// import BoardService from '../service/BoardService';
+
+const Product = () => {
+
+
+    return (
+    <div> test
+    </div>
+  )
+}
+export default Product;
