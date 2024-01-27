e4542dad39f3c006fec6069c9e0d3ad98d4e12f0
taejin_notebook <jinah0206@naver.com>
2024-01-27 13:14:38 +0900

    미르 commit

---
+++ DJMT/src/main/java/com/app/avengers/DJMT/mapper/product/ProductMapper.java
@@ -0,0 +1,12 @@
+package com.app.avengers.DJMT.mapper.product;
+
+import com.app.avengers.DJMT.dto.product.ProductDto;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface ProductMapper {
+
+    public List<ProductDto> getProduct();
+}
---
+++ DJMT/src/main/java/com/app/avengers/DJMT/service/product/ProductService.java
@@ -0,0 +1,24 @@
+package com.app.avengers.DJMT.service.product;
+
+import com.app.avengers.DJMT.dto.product.ProductDto;
+import com.app.avengers.DJMT.mapper.product.ProductMapper;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j
+@AllArgsConstructor
+@Service
+public class ProductService  {
+
+    private final ProductMapper mapper;
+
+    public List<ProductDto> getProduct() {
+        List<ProductDto> list = mapper.getProduct();
+        return list;
+    }
+
+
+}
