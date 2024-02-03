7433864eadeda99c9bbe5f4844981afad7692bcf
taejin <jinah0206@naver.com>
2024-02-03 18:52:46 +0900

    동기화작업중

--- DJMT/src/my-react-app/src/pages/member/view/test/Test.jsx
+++ DJMT/src/my-react-app/src/pages/member/view/test/Test.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Form from '../../common/Form';
 import * as yup from 'yup';
 import { loginHandler } from '../../handler/MemberHandler';
@@ -7,44 +7,68 @@ import { useRecoilState, useSetRecoilState } from 'recoil'
 import { useNavigate } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import { isLogin, memberKeyAtom, isAdmin } from '../../atom/LoginAtom'
+import { Avatar, Box } from '@mui/material';
 
 const Test = () => {
-    const [cookies, setCookie, removeCookie] = useCookies([])
-    const navigate = useNavigate()
-    const [isLoginCheck, setIsLoginCheck] = useRecoilState(isLogin) // 로그인 햇는지 체크
-    const setIsAdminCheck = useSetRecoilState(isAdmin) // 로그인 유저 Role 값
-    const setMemberKey = useSetRecoilState(memberKeyAtom) // MEM_NO 전역변수로 저장
+    const [Image, setImage] = useState(
+        'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png'
+      )
+    const fileInput = useRef(null)
+    const [myPageData, setMyPageData] = useState({})
 
-    const schema = yup.object().shape({
-        login_id: yup.string().required("ID is required"),
-        login_pw: yup
-          .string()
-          .required("Password is required"),
-    });
-    // 홈으로
-    const handleRedirect = e => {
-    e.preventDefault()
-    navigate('/')
+    const onChange = e => {
+    if (e.target.files[0]) {
+        setImage(e.target.files[0])
+        // setMyPageData({...myPageData,mem_profile : e.target.files[0]})
+        console.log( 'data > ', e.target.files[0])
+        console.log( myPageData)
+    } else {
+        //업로드 취소할 시
+        setImage(
+        'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png'
+        )
+        return
     }
-    // 로그인
-    const loginHandler1 = (data) => {
-        loginHandler(
-            data,
-            setIsLoginCheck,
-            setIsAdminCheck,
-            setMemberKey,
-            setCookie,
-            navigate
-          )
-      };
+    //화면에 프로필 사진 표시
+    const reader = new FileReader()
+    reader.onload = () => {
+        console.log('reander', reader)
+        if (reader.readyState === 2) {
+        setImage(reader.result)
+        console.log('result > ',reader.result)
+        // console.log('result > ',reader.result)
+        }
+    }
+    reader.readAsDataURL(e.target.files[0])
+
+    }
     return (
-        <div>
-            <Form className="login_form" schema={schema} onSubmit={loginHandler1}>
-                <Form.input  name="login_id" label="ID" type="text"/>
-                <Form.input  name="login_pw" label="password" type="password"/>
-                <Form.button className="login__btn" name="Sign in" type="submit"/>
-            </Form>
-        </div>
+        <Box
+            sx={{
+              marginTop: 8,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Avatar
+              src={Image}
+              style={{ margin: '20px', cursor: 'pointer' }}
+              sx={{ width: 100, height: 120 }}
+              onClick={() => {
+                fileInput.current.click()
+              }}
+            />
+            <input
+              type="file"
+              style={{ display: 'none' }}
+              accept="image/jpg,impge/png,image/jpeg"
+              name="mem_profile"
+              onChange={onChange}
+              
+              ref={fileInput}
+            />
+            </Box>
     );
 };
 
