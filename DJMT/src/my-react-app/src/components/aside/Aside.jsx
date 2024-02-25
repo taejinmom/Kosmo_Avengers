2440aec905b151ebdf7969d3caf856899f3912d8
dayoung_mac <h_chaos@naver.com>
2024-02-25 20:39:06 +0900

    모든페이지에서 찜목록 & 나만의코디 확인가능하도록 aside바(by.지혜)

---
+++ DJMT/src/my-react-app/src/components/aside/Aside.jsx
@@ -0,0 +1,20 @@
+import './Aside.css'
+
+function Aside() {
+  return (
+    <>
+      <aside>
+        <ul>
+          <li>
+            <a className='material-icons' href='javascript:void(0)'>checkroom</a>
+          </li>
+          <li>
+            <a className='material-icons red' href='javascript:void(0)'>favorite</a>
+          </li>
+        </ul>
+      </aside>
+    </>
+  )
+}
+
+export default Aside
